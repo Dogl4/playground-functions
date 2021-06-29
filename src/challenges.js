faf17ae57f850ff8d65791b22a67fbdca87b5c92
Dogl4 <doougllas@hotmail.com.br>
2021-06-29 20:03:23 -0300

    Adiciona o Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -51,8 +51,16 @@ function highestCount(numero) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let gato1 = Math.abs(mouse - cat1);
+  let gato2 = Math.abs(mouse - cat2);
+  if (gato1 > gato2) {
+    return 'cat2';
+  }
+  if (gato2 > gato1) {
+    return 'cat1';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
